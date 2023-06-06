c75b76a26fd7f85372ec727b7c11e0130addcc69
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2023-06-05 22:58:46 -0700

    added social post routes

--- src/routes/socialPostRoutes.ts
+++ src/routes/socialPostRoutes.ts
@@ -1,34 +1,63 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-//social Post
+//socialpost
 //create
-router.post('/', (req, res)=>{
-    res.status(501).json({error: 'not implemented'});
+router.post('/', async (req, res)=>{
+    const { content, image, user } = req.body;
+    console.log(req.body);
+    try{
+        const result = await prisma.socialPost.create({
+            data: {
+                content,
+                image,
+                user
+            },
+        });
+        console.trace(result);
+        res.json(result);
+    } catch (e) {
+        res.status(400).json({ error : "username and email should be unique" })
+    }
+
 });
 
 //list
-router.get('/', (req, res)=>{
-    res.status(501).json({error: 'not implemented'})
+router.get('/', async (req, res)=>{
+    const allPosts = await prisma.socialPost.findMany();
+    res.json(allPosts);
 })
 
-//get a social post
-router.get('/:id', (req, res) =>{
-    const {id} = req.params;
-    res.status(501).json({error: `not implemented: ${id}`})
+//get a socialPost
+router.get('/:id', async (req, res) =>{
+    const { id } = req.params;
+    const socialPost = await prisma.socialPost.findUnique({ where : {id :Number(id) } });
+    res.json(socialPost);
 })
 
-//update social post
-router.put('/:id', (req, res) =>{
-    const {id} = req.params;
-    res.status(501).json({error: `not implemented: ${id}`})
+//update socialPost
+router.put('/:id', async (req, res) =>{
+    const { id } = req.params;
+    const { content, image } = req.body;
+    try{
+        const result = await prisma.socialPost.update({
+            where: { id: Number(id)}, 
+            data: {content, image}
+        });
+        res.json(result);
+    } catch(e) {
+        res.status(400).json({error: `failed to update socialPost`});
+    }
 })
 
-//delete social post
-router.delete('/:id', (req, res) =>{
+//delete socialPost
+router.delete('/:id', async(req, res) =>{
     const {id} = req.params;
-    res.status(501).json({error: `not implemented: ${id}`})
+    await prisma.socialPost.delete({ where: {id: Number(id) } });
+    res.sendStatus(200);
 })
 
 export default router;
